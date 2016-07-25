8f0d7ea3e872e23176bdb4060c3fff21a07baef4
Anilam <troyanka-girl@yandex.ru>
2016-07-25 09:08:11 +0200

    Example Added + opcode script

--- opcode.py
+++ opcode.py
@@ -4,8 +4,6 @@ class Opcode:
 		self.operation = operation
 		self.bytes = bytes	
 
-x = Opcode('60','PUSH1',1)
-x2 = Opcode('61','PUSH2',2)
 lst = [
 Opcode('00','STOP',0),
 Opcode('01','ADD',0),
@@ -139,8 +137,7 @@ Opcode('f4','DELEGATECALL',0),
 Opcode('ff','SUICIDE',0),
 ]
 
-line = '6060604052361561007f576000357c0100000000000000000000000000000000000000000000000000000000900480632bc9ed02146100815780634e3b62ec146100a257806354fd4d50146100f45780638da5cb5b14610115578063b6549f751461014c578063c623674f14610159578063e322a468146101d25761007f565b005b61008c600450610288565b6040518082815260200191505060405180910390f35b6100f26004803590602001906004018035906020019191908080601f01602080910402602001604051908101604052809392919081815260200183838082843782019150505050505090506102a4565b005b6100ff60045061027f565b6040518082815260200191505060405180910390f35b6101206004506101f3565b604051808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b610157600450610399565b005b610164600450610219565b60405180806020018281038252838181518152602001915080519060200190808383829060006004602084601f0104600302600f01f150905090810190601f1680156101c45780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b6101dd60045061029b565b6040518082815260200191505060405180910390f35b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b6001600050805480601f0160208091040260200160405190810160405280929190818152602001826000508054801561027757820191906000526020600020905b81548152906001019060200180831161025a57829003601f168201915b505050505081565b60026000505481565b600360009054906101000a900460ff1681565b60046000505481565b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614801561030e5750600360009054906101000a900460ff16155b1561039557806001600050908051906020019082805482825590600052602060002090601f01602090048101928215610364579182015b82811115610363578251826000505591602001919060010190610345565b5b50905061038f9190610371565b8082111561038b5760008181506000905550600101610371565b5090565b50600050505b5b50565b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff161480156104035750600360009054906101000a900460ff16155b1561042c576001600360006101000a81548160ff02191690830217905550426004600050819055505b5b56'
-
+line = '6060'
 def ListSearch(lst, opcode):
 	for i, val in enumerate(lst):
 		if val.code == opcode :
@@ -157,21 +154,45 @@ def ReadByte(line, counter, length):
 		result = result + line[counter + i];
 	return result
 
-def OneOperation(line, counter, f):
+def OneOperation(line, counter, f):	
 	opcode = ReadByte(line, counter, 1)
 	a = ListSearch(lst, opcode)
 	counter = counter + 2
-	f.write(a.operation +' ' + ReadByte(line, counter, a.bytes) + '\n') 
-	print a.operation +' ' + ReadByte(line, counter, a.bytes) 
-	counter = counter + 2 * a.bytes
+	if (a == None):
+		f.write(opcode + 'Is not found')
+	else:
+		f.write(a.operation +' ' + ReadByte(line, counter, a.bytes) + '\n') 
+		counter = counter + 2 * a.bytes
 	return counter
 
 def Parse (line, address):
-	f = open(address,'w')
-	counter = 0
-	while counter < len(line):
-		counter = OneOperation(line, counter, f)
-	f.close()
-		
+	try:
+		f = open(address,'w')
+		counter = 0
+		while counter < len(line):
+			counter = OneOperation(line, counter, f)
+		f.close()
+	except: 
+		print("Error while writing to the file " + address)
 
-Parse(line, '/home/anilam/temp/temp')
+def main():
+	import argparse
+	parser = argparse.ArgumentParser(description='Contract parser')
+	parser.add_argument('inputAddress', metavar='input', type=str, 
+                    help='address from where to read')
+	parser.add_argument('outputAddress', metavar = 'output', type = str, help = 'address to where to write')
+	args = parser.parse_args()
+	inputAddress = args.inputAddress
+	outputAddress = args.outputAddress
+	line = ''
+	try:
+		f = open(inputAddress,'r')
+		line = f.read()
+		line = line[0:len(line)-1]
+	except:
+		print ("Exception while opening")
+	Parse(line,outputAddress)
+	
+				
+main()
+#Parse(line, '/home/anilam/temp/temp')
